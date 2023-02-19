63746a17deada148ec0c78ca7f497e7bdcb2fb6f
jrvieira <github@jrvieira.com>
2023-02-19 04:15:22 +0000

    clean

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,11 @@ module Main where
 
 import Data.Bifunctor ( bimap )
 import Control.Monad ( join )
-
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Interact
 
-w, h :: Float
-w = 200
-h = 200
-
 main :: IO ()
-main = play (InWindow "mach sim" (round w,round h) (0, 0)) (makeColorI 0 0 0 0) 30 state render catch step
+main = play (InWindow "mach sim" (join (,) $ round size) (0, 0)) (makeColorI 0 0 0 0) fps state render catch step
 
 data State = State Float (Float,Float)
 
@@ -19,18 +14,48 @@ state :: State
 state = State 0 (0.0,0.0)
 
 render :: State -> Picture
-render (State t pos) = Pictures [tx,ty,curve]
+render (State δ pos) = Pictures [tx,ty,tr,curve]
    where
-   (x,y) = join bimap (fromInteger . round . (/ 10)) pos
-   tx = translate 0 -100 $ color white $ scale 0.1 0.1 $ text $ show (round x)
-   ty = translate -100 0 $ color white $ scale 0.1 0.1 $ text $ show (round y)
-   curve = color green $ line $ zip xs ys
-   xs = map ((* 60) . sin . (* x)) (take 1000 $ (+ (t / max x y)) <$> [0,0.01..])
-   ys = map ((* 60) . cos . (* y)) (take 1000 $ (- (t / max x y)) <$> [0,0.01..])
+   (x,y) = join bimap (succ . round . (/ (size / steps)) . (+ size/2)) pos
+   (x',y') = join bimap fromIntegral (x,y)
+   tx = translate 0 (-size/2) $ color white $ scale 0.1 0.1 $ text $ show x
+   ty = translate (-size/2) 0 $ color white $ scale 0.1 0.1 $ text $ show y
+   tr = translate (-size/2) (-size/2) $ color white $ scale 0.1 0.1 $ text $ show (div y $ gcd x y) <> ":" <> show (div x $ gcd x y)
+   curve = color (makeColor 0 1 0 intensity) $ line $ zip xs ys
+   xs = (* (zoom * size/2)) . sin . (* x') . (- (δ / max x' y')) <$> [0,res..2*pi]
+   ys = (* (zoom * size/2)) . cos . (* y') . (+ (δ / max x' y')) <$> [0,res..2*pi]
 
 catch :: Event -> State -> State
-catch (EventMotion pos) (State t _) = State t pos
+catch (EventMotion pos) (State δ _) = State δ pos
 catch _ s = s
 
 step :: Float -> State -> State
-step _ (State t pos) = State (t + 0.01) pos
+step _ (State δ pos) = State (δ + speed) pos
+
+-- size of canvas in pixels
+size :: Float
+size = 200
+
+-- line resolution (lower is better)
+res :: Float
+res = 1/100
+
+-- curve size as ratio of canvas
+zoom :: Float
+zoom = 0.7
+
+-- x/y cycles
+steps :: Float
+steps = 9
+
+fps :: Int
+fps = 30
+
+-- animation speed (rotation per frame in radians)
+speed :: Float
+speed = 1/100
+
+-- line brightness
+intensity :: Float
+intensity = 0.2
+
