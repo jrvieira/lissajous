9f05b83452aa898c6bbc494c6cd12d8b3e5bd130
jrvieira <github@jrvieira.com>
2023-02-22 21:03:48 +0000

    linear mode + controls

--- app/Main.hs
+++ app/Main.hs
@@ -1,59 +1,96 @@
 module Main where
 
-import Data.Bifunctor ( bimap )
-import Control.Monad ( join )
+import Data.Fixed ( mod')
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Interact
 
-data State = State Float (Float,Float)
+data State = State
+   { δ :: Float  -- radian time
+   , φ :: (Word,Word)  -- input frequencies
+   , σ :: Float  -- radian phase shift per frame
+   , ρ :: Word  -- resolution
+   , α :: Float  -- amplitude
+   , β :: Float  -- brightness
+   , γ :: Float  -- rational grid
+   , ω :: Waveform
+   , κ :: Bool  -- sc
+   }
+
+state :: State
+state = State
+   { δ = 0
+   , φ = (1,1)
+   , σ = 1/100
+   , ρ = 16
+   , α = 7/10
+   , β = 0.2
+   , γ = 9
+   , ω = Sine
+   , κ = False
+   }
+
+data Waveform = Sine | Square | Triangle | Saw
 
 main :: IO ()
-main = play (InWindow "mach sim" (join (,) $ round size * 2) (0,0)) (makeColorI 0 0 0 0) fps (State 0 (0,0)) render catch step
+main = play (InWindow "mach sim" (round $ size * 4,round $ size * 2) (0,0)) (makeColorI 0 0 0 0) fps state render catch step
 
 render :: State -> Picture
-render (State δ pos) = Pictures [tx,ty,tr,curve]
+render st = Pictures [tx,ty,tr,curve,sx,sy]
    where
-   (x,y) = join bimap (succ . round . (/ (size * 2 / steps)) . (+ size)) pos
-   (x',y') = join bimap fromIntegral (x,y)
-   tx = translate 0 -size $ color white $ scale 0.1 0.1 $ text $ show x
-   ty = translate -size 0 $ color white $ scale 0.1 0.1 $ text $ show y
-   tr = translate -size -size $ color white $ scale 0.1 0.1 $ text $ show (div y $ gcd x y) <> ":" <> show (div x $ gcd x y)
-   curve = color (makeColor 0 1 0 intensity) $ lineLoop $ zip xs ys
-   xs = (* zoom) . sin . (* x') . (- δ * sc) <$> [0,res..2*pi]
-   ys = (* zoom) . cos . (* y') . (+ δ * sc) <$> [0,res..2*pi]
-   sc = 1 / fromIntegral (lcm x y)  -- speed coefficient (slow down more complex shapes)
+   (x,y :: Word) = φ st
+   tx = translate  -size      -size       $ color white $ scale 0.07 0.07 $ text $ show x
+   ty = translate (-size * 2)  0          $ color white $ scale 0.07 0.07 $ text $ show y
+   tr = translate (-size * 2) -size       $ color white $ scale 0.07 0.07 $ text $ show (div y $ gcd x y) <> ":" <> show (div x $ gcd x y)
+   curve = translate -size 0 $ color (makeColor 0 1 0 (β st)) $ lineLoop $ zip xs ys
+   xs = (* (α st * size)) . fun . (* fromIntegral x) . (- δ st * sc) <$> init [0,2*pi / fromIntegral res..2*pi]
+   ys = (* (α st * size)) . fun . (* fromIntegral y) . (+ δ st * sc) <$> init [0,2*pi / fromIntegral res..2*pi]
+   --    ^scale                    ^periods             ^rotation
+
+   res = ρ st ^ 2  -- resolution multiplier
+   sc | κ st = 1 / fromIntegral (lcm x y)
+      | otherwise = 1  -- speed coefficient (slow down more complex shapes)
+
+   sx = translate 0 0 $ color (makeColor 1 0.5 0 (β st)) $ line $ zip [0,2 * size / fromIntegral res..] xs
+   sy = translate 0 0 $ color (makeColor 0 0.5 1 (β st)) $ line $ zip [0,2 * size / fromIntegral res..] ys
+
+   fun
+      | Sine <- ω st = sin
+      | Square <- ω st = \v' -> let v = abs $ mod' v' pi in if v < pi / 2 then 1 else -1
+      | Triangle <- ω st = \v' -> let v = abs $ mod' v' (2*pi) in if v < pi then 2 * (v / pi - 1) + 1 else -2 * (v / pi - 1) + 1
+      | Saw <- ω st = \v' -> let v = abs $ mod' v' (2*pi) in v / pi - 1
 
 catch :: Event -> State -> State
-catch (EventMotion pos) (State δ _) = State δ pos
+-- catch (EventMotion c) st = st { ξ = c }
+catch (EventKey k Down _ _) st
+   | Char '0' <- k = state { φ = φ st , ω = ω st }
+   | Char 'x' <- k = st { φ = let (x,y) = φ st in (min maxBound $ succ x,y) }
+   | Char 'X' <- k = st { φ = let (x,y) = φ st in (max 1 $ pred x,y) }
+   | Char 'y' <- k = st { φ = let (x,y) = φ st in (x,min maxBound $ succ y) }
+   | Char 'Y' <- k = st { φ = let (x,y) = φ st in (x,max 1 $ pred y) }
+   | Char 's' <- k = st { σ = σ st + 0.01 }
+   | Char 'S' <- k = st { σ = σ st - 0.01 }
+   | Char 'r' <- k = st { ρ = min maxBound $ succ $ ρ st }
+   | Char 'R' <- k = st { ρ = max 1 $ pred $ ρ st }
+   | Char 'a' <- k = st { α = α st + 0.1 }
+   | Char 'A' <- k = st { α = α st - 0.1 }
+   | Char 'b' <- k = st { β = β st + 0.01 }
+   | Char 'B' <- k = st { β = β st - 0.01 }
+   | Char 'g' <- k = st { γ = succ $ γ st }
+   | Char 'G' <- k = st { γ = pred $ γ st }
+   | Char 'q' <- k = st { ω = Square }
+   | Char 'w' <- k = st { ω = Saw }
+   | Char 'e' <- k = st { ω = Sine }
+   | Char 't' <- k = st { ω = Triangle }
+   | Char 'k' <- k = st { κ = True }
+   | Char 'K' <- k = st { κ = False }
 catch _ s = s
 
 step :: Float -> State -> State
-step _ (State δ pos) = State (δ + speed) pos
+step _ st = st { δ = δ st + σ st }
 
 -- size of quadrant in pixels
 size :: Float
 size = 100
 
--- line resolution (lower is better)
-res :: Float
-res = 1/90
-
--- curve size
-zoom :: Float
-zoom = 0.7 * size
-
--- x/y cycles
-steps :: Float
-steps = 9
-
 fps :: Int
 fps = 30
-
--- rotation per frame in radians (lower is slower)
-speed :: Float
-speed = 1/100
-
--- line brightness
-intensity :: Float
-intensity = 0.2
-
